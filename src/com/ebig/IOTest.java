a0df7a873e6ec0509eb71b6e6f47e2633660ff16
zhang <775647651@qq.com>
2022-01-14 14:57:22 +0800

    配置文件

--- src/com/ebig/IOTest.java
+++ src/com/ebig/IOTest.java
@@ -4,9 +4,9 @@ import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.io.Serializable;
 
+//
 public class IOTest implements Serializable {
     private final static long uid = 123123123214412241L;
-    File
     InputStream in = new BufferedInputStream();
 }
 
