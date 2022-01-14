d39cd88904f92b72108e47b61a9538422ed2e5df
zhang <775647651@qq.com>
2022-01-14 17:07:07 +0800

    配置文件

--- src/com/ebig/IOTest/SerializableGo.java
+++ src/com/ebig/IOTest/SerializableGo.java
@@ -1,5 +1,29 @@
 package com.ebig.IOTest;
 
+import com.ebig.entity.Car;
+import com.ebig.entity.Person;
+
+import java.io.*;
+
 public class SerializableGo {
+    public static <T extends Serializable> T clone(T man) throws IOException,ClassNotFoundException{
+        ByteArrayOutputStream bot = new ByteArrayOutputStream();
+        ObjectOutputStream oot = new ObjectOutputStream(bot);
+        oot.writeObject(man);
+        ByteArrayInputStream bit = new ByteArrayInputStream(bot.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bit);
+        return (T)ois.readObject();
+    }
+
+    public static void main(String[] args) throws Exception{
+        Person p = new Person("张",213,new Car("宝马",55));
+        Person go = SerializableGo.clone(p);
+        System.out.println(p);
+        System.out.println(go);
+        p.setAge(1);
+        System.out.println(p);
+        System.out.println(go);
+    }
+
 
 }
